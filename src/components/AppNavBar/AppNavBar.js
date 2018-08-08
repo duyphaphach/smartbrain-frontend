dfe01d5cc710a39a1396ebfbfbe340c6349daa0d
duyphaphach <duy@passcareer.io>
2018-08-08 10:33:41 +0700

    Add mdbootstrap and SignInform

--- src/components/AppNavBar/AppNavBar.js
+++ src/components/AppNavBar/AppNavBar.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import AppLogo from '../AppLogo/AppLogo.js';
-import Login from '../Login/login.js';
+// import Login from '../Login/login.js';
 import FaceReco from '../FaceRecognition/FaceRecognition.js';
 import DemoGraph from '../DemoGraphics/demoGraphics.js';
 import {Navbar, Nav, NavItem, Button} from 'react-bootstrap';
