bcdfefa617c41eed975585a8af2c752366a2ecb8
Huy <huy@passcareer.io>
2018-08-09 17:48:59 +0700

    remove tachyons

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ import Clarifai from 'clarifai';
 import {Image, Row, Col} from 'react-bootstrap';
 
 const app = new Clarifai.App({
- apiKey: '6b17ac395a4a482f87c614a73c04971c'
+ apiKey: '2ca0cad8e48c4f23a69afa2ac1bcbfce'
 });
 
 class FaceReco extends React.Component {
@@ -15,8 +15,25 @@ class FaceReco extends React.Component {
       imageUrl: '',
       input: '',
       box: {},
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+loadUser = (data) => {
+  this.setState({user: {
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    entries: data.entries,
+    joined: data.joined
+  }})
+}
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -47,7 +64,20 @@ class FaceReco extends React.Component {
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(response => {
-        console.log(response);
+        if (response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
+        }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch(err => console.log(err, 'ad'));
@@ -59,9 +89,9 @@ class FaceReco extends React.Component {
     return (
       <div>
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <div className='center ma tc'>
+        <div className='center ma' style={{margin: '0 auto'}}>
             <div className = 'absolute mt2'>
-              <img atl="" id="inputimage" src={imageUrl} width='500px' height='auto' style={{margin: 'center'}}/>
+              <img atl="" id="inputimage" src={imageUrl} width='500px' height='auto'/>
               <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
